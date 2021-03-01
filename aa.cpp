31b1312b7e1b29a523edaf830918fe1b02dc64a1
harbang <hsu9643@naver.com>
2021-03-01 23:14:31 +0900

    suwon:third

--- aa.cpp
+++ aa.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "bb.cpp"
 
 int test(){
     printf("test");
 }
 
 int main(){
+    Suwon suwon;
     printf("Test");
 }
---
+++ bb.cpp
@@ -0,0 +1,7 @@
+class Node{
+    int id;
+};
+
+class Suwon{
+    int id;
+};
