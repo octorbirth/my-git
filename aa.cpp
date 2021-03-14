a43f0b06a70ce072a30a5b2c26f3040e77500e52
harbang <hsu9643@naver.com>
2021-03-14 15:40:57 +0900

    15:40

---
+++ aa.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "bb.cpp"
+
+int test(){
+    printf("test");
+    printf("==============");
+}
+
+int main(){
+    Suwon suwon;
+    printf("Test");
+    printf("Test");
+    printf("Test");
+    printf("Test3");
+    printf("Test4");
+}
