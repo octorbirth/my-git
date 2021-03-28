c2c4feae668f23606d797e87806f66f2c901f2a5
harbang <hsu9643@naver.com>
2021-03-28 19:06:37 +0900

    fw: commit

--- test.cpp
+++ test.cpp
@@ -4,4 +4,9 @@ int main(){
     printf("After Push, Not Merged")
     printf("222 After Commit, Not push");
     printf("333 After Commit, Not push");
+
+
+    printf("444 After Add, Not Commit");
+    printf("555 After Add, Not Commit");
+    printf("666 Not Add");
 }
