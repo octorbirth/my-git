c91fb3c19d80eef3e3b0565d3635bcd367fc52c3
sw.hyun <hsu9643@gmail.com>
2022-05-07 22:25:45 +0900

    cherry-pick 1
    Signed-off-by: sw.hyun <hsu9643@gmail.com>

--- aa.cpp
+++ aa.cpp
@@ -10,11 +10,6 @@ aa
 
 int main(){
     Suwon suwon;
-    printf("Test");
-    printf("Test");
-    printf("Test");
-    printf("Test3");
-    printf("Test4");
 
     printf("Test");
     printf("Test");
